69755488e22c407d86edc9bdb78a45d3369b91d5
Andres <ballerlikemike23@gmail.com>
2018-06-19 23:52:06 -0400

    calcPerecentage function

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var budgetController = (function() {
     this.id = id;
     this.description = description;
     this.value = value;
+    this.percentage = -1;
   };
+
+  Expense.prototype.calcPercentage = function(totalIncome) {
+
+    this.percentage = Math.round((this.value / totalIncome) * 100)
+  }
 
   var Income = function (id, description, value) {
     this.id = id;
@@ -88,6 +94,17 @@ var budgetController = (function() {
       } else {
         data.percentage = -1;
       }
+    },
+
+    calculatePercentages: function() {
+
+      /* 
+      a=20
+      b=10
+      c=40
+      income = 100
+      a=20/100= 20%
+      */
     },
 
     getBudget: function() {
